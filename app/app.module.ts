09320455385ddb09c5114a3413e683838d26ff2b
Kyle Bustard <bustardkyle@gmail.com>
2017-01-26 16:50:45 -0500

    hello world

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+import { HomepageComponent }  from './homepage.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ HomepageComponent ],
+  bootstrap:    [ HomepageComponent ]
 })
 export class AppModule { }
