05432db7cb79ad4ec05fa1a6ed4ee1720e473400
Kyle Bustard <bustardkyle@gmail.com>
2017-01-26 17:48:59 -0500

    implement router module

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  moduleId: module.id,
+  selector: 'app',
+  template: `
+
+  <h1>Navigation</h1>
+
+  <router-outlet></router-outlet>
+  `
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+	title: 'Freelance Dashboard';
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,22 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { HomepageComponent }  from './homepage.component';
+import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ HomepageComponent ],
-  bootstrap:    [ HomepageComponent ]
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    HomepageComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
