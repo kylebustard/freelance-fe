b73f67d4ebfc01306ff23bb65c1c01ee4c7ec0d5
Kyle Bustard <bustardkyle@gmail.com>
2017-01-30 22:18:21 -0500

    create mock data in angular2 application

--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Document } from './document';
 
 @Component({
 	moduleId: module.id,
@@ -7,5 +8,27 @@ import { Component } from '@angular/core';
 })
 
 export class DocumentsComponent {
-	
+	documents: Document[] = [
+		{
+	    title: 'My First Doc',
+	    description: 'lorem lipsum', 
+	    file_url: 'http://google.com',
+	    updated_at: '1/30/16',
+	    image_url: 'http://google.com',
+	  }
+	  {
+	    title: 'My Second Doc',
+	    description: 'lorem lipsum', 
+	    file_url: 'http://google.com',
+	    updated_at: '1/30/16',
+	    image_url: 'http://google.com',
+	  }
+	  {
+	    title: 'My Last Doc',
+	    description: 'lorem lipsum', 
+	    file_url: 'http://google.com',
+	    updated_at: '1/30/16',
+	    image_url: 'http://google.com',
+	  }
+	]
 }
