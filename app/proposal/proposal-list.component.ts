ed165ea2dd2cfb2b733fe7bf3788321ef27c039d
Kyle Bustard <bustardkyle@gmail.com>
2017-01-31 21:58:24 -0500

    populate Angular proposal-list view with data

--- app/proposal/proposal-list.component.ts
+++ app/proposal/proposal-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Proposal } from './proposal';
 
 @Component({
 	moduleId: module.id,
@@ -8,5 +9,40 @@ import { Component } from '@angular/core';
 })
 
 export class ProposalListComponent {
-	
+	proposalOne: Proposal = new Proposal(
+	  15,
+	  'Acme Company',
+	  'http://kylebustard.github.io',
+	  'Ruby on Rails',
+	  150,
+	  120,
+	  15,
+	  'bustardkyle@gmail.com'
+	)
+	proposalTwo: Proposal = new Proposal(
+	  33,
+	  'XYZ Company',
+	  'http://kylebustard.github.io',
+	  'Ruby on Rails',
+	  150,
+	  120,
+	  15,
+	  'bustardkyle@gmail.com'
+	)
+	proposalThree: Proposal = new Proposal(
+	  300,
+	  'ABC Company',
+	  'http://kylebustard.github.io',
+	  'Ruby on Rails',
+	  150,
+	  120,
+	  15,
+	  'bustardkyle@gmail.com'
+	)
+
+	proposals: Proposal[] = [
+    this.proposalOne,
+    this.proposalTwo,
+    this.proposalThree
+	]
 }
